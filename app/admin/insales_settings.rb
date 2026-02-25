f3defd51d2b86230ecd11a355ff2d6275e10a362
Артём Санжаревский <artemyan@Artems-MacBook-Pro.local>
2026-02-25 23:56:52 +0300

    Fix InSales Settings checkboxes

--- app/admin/insales_settings.rb
+++ app/admin/insales_settings.rb
@@ -68,14 +68,14 @@ ActiveAdmin.register InsalesSetting do
       f.input :category_id
       f.input :default_collection_id
       f.input :image_url_mode, as: :select, collection: %w[service_url rails_url], include_blank: false
-      f.input :skip_products_without_sku, label: 'Не загружать товары без артикула'
-      f.input :skip_products_with_nonpositive_stock, label: 'Не загружать товары с нулевым остатком (ноль и меньше нуля)'
+      f.input :skip_products_without_sku, as: :boolean, label: 'Не загружать товары без артикула'
+      f.input :skip_products_with_nonpositive_stock, as: :boolean,
+                                                      label: 'Не загружать товары с нулевым остатком (ноль и меньше нуля)'
       f.input :allowed_store_names,
               as: :select,
               collection: store_names,
               input_html: { multiple: true },
               hint: 'Склады, которые участвуют в экспорте в InSales. Если пусто — используется "Тест".'
-      f.input :cached_store_names_synced_at, label: 'Склады обновлены', input_html: { disabled: true }
     end
 
     f.actions
